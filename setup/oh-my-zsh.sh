0022d5de8c2be3552af36c9b5f93580cd556abaf
Fabio Greter <fabio.greter@bluewin.ch>
2022-12-24 00:12:26 +0100

    Fix oh my zsh setup script

--- setup/oh-my-zsh.sh
+++ setup/oh-my-zsh.sh
@@ -4,4 +4,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 ### Plugins ###
 ## history-substring-search ##
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-sed -i -E 's/plugins=\((.*)\)/plugins=\(\1 history-substring-search\)/g' test
+sed -i -E 's/plugins=\((.*)\)/plugins=\(\1 history-substring-search\)/g' /.zshrc
+
+cat ~/.zshrc
