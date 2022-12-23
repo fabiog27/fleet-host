e17757db4f53c08db9e4ec56a3d897162706de31
Fabio Greter <fabio.greter@bluewin.ch>
2022-12-23 23:17:32 +0100

    Reformat asdf deps file

--- install-asdf-deps.sh
+++ install-asdf-deps.sh
@@ -6,7 +6,9 @@ apk add tar gpg gpg-agent
 # Python #
 apk add build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev curl llvm \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libsasl2-dev python3-dev libldap2-dev
+    libncursesw5-dev xz-utils tk-dev libxml2-dev \
+    libxmlsec1-dev libffi-dev liblzma-dev libsasl2-dev \
+    python3-dev libldap2-dev
 
 # Ruby #
 apk add bash curl git musl-locales \
@@ -15,4 +17,3 @@ apk add bash curl git musl-locales \
 
 # Java #
 apk add bash curl unzip jq
-
