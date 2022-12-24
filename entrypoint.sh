f196605a822460476d29cbe1d03b3f623a378f52
Fabio Greter <fabio.greter@bluewin.ch>
2022-12-24 12:43:34 +0100

    Adapt entrypoint.sh to use existing ssh keys (from volume mount) instead of CLI arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,39 @@
 #!/bin/zsh
 
-zparseopts -D \
-    -git-repo:=o_git_repo \
-    -ssh-key:=o_ssh_key
+zparseopts -D -git-repo:=o_git_repo
 
 PROJECT_DIR=/home/fleet/workspace
 SSH_KEY=
 
 if [ -n "${o_git_repo[2]+1}" ]; then
+
     echo "Specified git repo: ${o_git_repo[2]}"
+
     HOST_NAME=$(echo $o_git_repo[2] | sed -E 's/^.*@(.*):.*$/\1/')
-    FOLDER_NAME=$(echo $o_git_repo[2] | sed -E 's/^.*@.*:.*/(.*)\.git$/\1/')
+    FOLDER_NAME=$(echo $o_git_repo[2] | sed -E 's/^.*@.*:.*\/(.*)\.git$/\1/')
     echo "Host name: ${HOST_NAME}"
     echo "Folder name: ${FOLDER_NAME}"
+
     SSH_KEYSCAN_OUTPUT=$(ssh-keyscan -H $HOST_NAME)
-    echo "Keyscan output:"
-    echo $SSH_KEYSCAN_OUTPUT
+#    echo "Keyscan output:"
+#    echo $SSH_KEYSCAN_OUTPUT
     mkdir -p ~/.ssh
     echo $SSH_KEYSCAN_OUTPUT >> ~/.ssh/known_hosts
+
     PROJECT_DIR=$(echo "$(pwd)/${FOLDER_NAME}")
-    if [ -n "${o_ssh_key[2]+1}" ]; then
-        echo "Using ssh key: ${o_ssh_key[2]}"
-        echo $o_ssh_key[2] > /home/fleet/.ssh/id_git
+
+    if [ -f ~/.ssh/id_git && -f ~/.ssh/id_git.pub ]; then
+
+        echo "Using ssh key"
         chmod 600 /home/fleet/.ssh/id_git
-        SSH_KEY=$o_ssh_key[2]
+        chmod 600 /home/fleet/.ssh/id_git.pub
         git clone $o_git_repo[2] --config core.sshCommand="ssh -i /home/fleet/.ssh/id_git"
+
     else
+
+        echo "Proceeding to clone without ssh key"
         git clone $o_git_repo[2]
+
     fi
 fi
 
