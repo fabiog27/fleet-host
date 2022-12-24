caab8ae99bcbb64de3577c35810d76a5dd4fe0aa
Fabio Greter <fabio.greter@bluewin.ch>
2022-12-24 11:17:06 +0100

    Add debug info to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,18 @@ SSH_KEY=
 if [ -n "${o_git_repo[2]+1}" ]; then
     echo "Specified git repo: ${o_git_repo[2]}"
     HOST_NAME=$(cat o_git_repo | sed -E 's/^.*@(.*):.*$/\1/')
-    ssh-keyscan -H $HOST_NAME >> ~/.ssh/known_hosts
+    echo "Host name: ${HOST_NAME}"
+    SSH_KEYSCAN_OUTPUT=$(ssh-keyscan -H $HOST_NAME)
+    echo "Keyscan output:"
+    echo $SSH_KEYSCAN_OUTPUT
+    mkdir -p ~/.ssh
+    echo $SSH_KEYSCAN_OUTPUT >> ~/.ssh/known_hosts
     PROJECT_DIR=$(echo "$(pwd)/${o_git_repo[2]}")
     if [ -n "${o_ssh_key[2]+1}" ]; then
         echo "Using ssh key: ${o_ssh_key[2]}"
-        mkdir -p ~/.ssh
         echo $o_ssh_key[2] > /home/fleet/.ssh/id_git
         SSH_KEY=$o_ssh_key[2]
-        git clone $o_git_repo[2] --config core.sshCommand="ssh -i /home/flet/.ssh/id_git"
+        git clone $o_git_repo[2] --config core.sshCommand="ssh -i /home/fleet/.ssh/id_git"
     else
         git clone $o_git_repo[2]
     fi
