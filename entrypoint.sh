bef6bb537a3b9d286cd8ee5d34ef468ed63f4211
Fabio Greter <fabio.greter@bluewin.ch>
2022-12-24 11:25:00 +0100

    Fix git folder name

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,15 @@ SSH_KEY=
 if [ -n "${o_git_repo[2]+1}" ]; then
     echo "Specified git repo: ${o_git_repo[2]}"
     HOST_NAME=$(echo $o_git_repo[2] | sed -E 's/^.*@(.*):.*$/\1/')
+    FOLDER_NAME=$(echo $o_git_repo[2] | sed -E 's/^.*@.*:.*/(.*)\.git$/\1/')
     echo "Host name: ${HOST_NAME}"
+    echo "Folder name: ${FOLDER_NAME}"
     SSH_KEYSCAN_OUTPUT=$(ssh-keyscan -H $HOST_NAME)
     echo "Keyscan output:"
     echo $SSH_KEYSCAN_OUTPUT
     mkdir -p ~/.ssh
     echo $SSH_KEYSCAN_OUTPUT >> ~/.ssh/known_hosts
-    PROJECT_DIR=$(echo "$(pwd)/${o_git_repo[2]}")
+    PROJECT_DIR=$(echo "$(pwd)/${FOLDER_NAME}")
     if [ -n "${o_ssh_key[2]+1}" ]; then
         echo "Using ssh key: ${o_ssh_key[2]}"
         echo $o_ssh_key[2] > /home/fleet/.ssh/id_git
