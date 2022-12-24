bc09f08cdc88ac9c57c7b56abf87a59e9ce77887
Fabio Greter <fabio.greter@bluewin.ch>
2022-12-24 12:19:40 +0000

    Add more plugins for oh-my-zsh

--- setup/oh-my-zsh.sh
+++ setup/oh-my-zsh.sh
@@ -4,6 +4,14 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 ### Plugins ###
 ## history-substring-search ##
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-sed -i -E 's/plugins=\((.*)\)/plugins=\(\1 history-substring-search\)/g' /.zshrc
+sed -i -E 's/plugins=\((.*)\)/plugins=\(\1 zsh-history-substring-search\)/g' /home/fleet/.zshrc
 
-cat ~/.zshrc
+## autosuggestions ##
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i -E 's/plugins=\((.*)\)/plugins=\(\1 zsh-autosuggestions\)/g' /home/fleet/.zshrc
+
+## syntax-highlighting ##
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i -E 's/plugins=\((.*)\)/plugins=\(\1 zsh-syntax-highlighting\)/g' /home/fleet/.zshrc
+
+cat ~/.zshrc | grep plugins=
